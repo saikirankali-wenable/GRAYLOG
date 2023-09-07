1eba3b82262c67ad3a6fbb21a382a01b05606ca5
saikirankali-wenable <saikiran.kali@wenable.com>
2023-09-07 15:14:56 +0530

    updated user data

--- ec2.tf
+++ ec2.tf
@@ -12,8 +12,17 @@ resource "aws_instance" "graylog_instance" {
         delete_on_termination = true 
     }
     user_data = <<-EOF
+             #!/bin/bash
+
+             echo "installing java and mongoDB"
              apt-get install apt-transport-https openjdk-8-jre-headless uuid-runtime pwgen
+             
+             wget -qO - https://www.mongodb.org/static/pgp/server-4.4.asc | sudo apt-key add -
+             echo "deb [ arch=amd64 ] https://repo.mongodb.org/apt/ubuntu bionic/mongodb-org/4.4 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-4.4.list
+             apt-get update
              apt-get install mongodb-server
+
+             echo installing "installing ElasticSearch"
              wget -qO – https://artifacts.elastic.co/GPG-KEY-elasticsearch | apt-key add –
              echo “deb https://artifacts.elastic.co/packages/5.x/apt stable main” | tee -a /etc/apt/sources.list.d/elastic-5.x.list
              apt-get update && apt-get install elasticsearch
@@ -22,10 +31,11 @@ resource "aws_instance" "graylog_instance" {
              action.auto_create_index: false
              EOT
 
-
              systemctl daemon-reload
              systemctl enable elasticsearch.service
              systemctl restart elasticsearch.service
+             
+             echo "installing  Graylog"
              
              wget https://packages.graylog2.org/repo/packages/graylog-2.5-repository_latest.deb
              dpkg -i graylog-2.5-repository_latest.deb
